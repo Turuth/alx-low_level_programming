8814b43f87611ce699e7e2c36fae1091eabd7087
Turuth <nehetworuthi@gmail.com>
2023-07-14 02:57:59 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - using printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
