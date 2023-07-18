3dcdeb2ebb9d665079b9d4d702dca591f8eab534
Turuth <nehetworuthi@gmail.com>
2023-07-18 13:28:58 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabets in lowercase
+ */
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
