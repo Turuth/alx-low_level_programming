99f48a1b315e76aadb08d94919eeb79ca6a9a746
Turuth <nehetworuthi@gmail.com>
2023-07-24 15:20:56 -0700

    mbogi

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*reset_to_98 - takes a pointer to a parameter and updates the value of n
+*@n:shows integer
+*Return: no return
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
